fa82e9d41cad3a0c5b40b4c4e3bebddee307cdbd
AdrienLagache <lagacheadrien64@gmail.com>
2024-01-16 20:27:03 +0100

    [STYLE] add responsive header-logo

--- src/components/HeaderLogo/HeaderLogo.jsx
+++ src/components/HeaderLogo/HeaderLogo.jsx
@@ -7,7 +7,7 @@ function HeaderLogo() {
   const themeIndex = useSelector((state) => state.theme.themeIndex);
 
   return (
-    <div className={`header-logo__${themesList[themeIndex]}`}>
+    <div className={`header-logo header-logo__${themesList[themeIndex]}`}>
       {/* <h1 className="header-logo__letter" id="a-letter">
         A<span id="firstname">drien</span>
       </h1>
