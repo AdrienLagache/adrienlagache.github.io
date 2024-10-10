19776e90ddf5d06bd76091b090959f2380ad36df
Adrien Lagache <lagacheadrien64@gmail.com>
2024-10-10 14:01:50 +0200

    [refactor] clean dist file

--- src/utils/datas.js
+++ src/utils/datas.js
@@ -1,17 +1,6 @@
 export default [
   {
     id: 1,
-    name: 'cfdr',
-    title:
-      '<span>C.F.D.R.</span> - <strong>PHP</strong>, <strong>MVC</strong>, <strong>Composer</strong>',
-    url: 'https://cfdr.fr',
-    description:
-      "Ce site sert d'interface aux organisateurs d'un championnat e-sport afin d'afficher les classements, calendriers et line-up des différentes saisons",
-    details:
-      "Il a été réalisé en <strong>PHP</strong> à partir d'une architecture <strong>MVC</strong>.<br/><strong>Composer</strong> a été utilisé comme gestionnaire de dépendance pour AltoRouter et AltoDispatcher.",
-  },
-  {
-    id: 2,
     name: 'maptalents',
     title:
       '<span>MapTalents</span> - <strong>REACT</strong>, <strong>REDUX</strong>, <strong>NestJS</strong>',
@@ -22,7 +11,29 @@ export default [
       'Il a été réalisé avec <strong>REACT</strong> et <strong>Redux</strong> pour la partie front-end et avec <strong>NestJS</strong> pour la partie back-end.<br/>Nous avons travaillé en équipe de 4 et mes réalisations principales sont la partie inscription (front + back) et la gestion des filtres de recherche (back).',
   },
   {
+    id: 2,
+    name: 'cfdr',
+    title:
+      '<span>C.F.D.R.</span> - <strong>PHP</strong>, <strong>MVC</strong>, <strong>Composer</strong>',
+    url: 'https://cfdr.fr',
+    description:
+      "Ce site sert d'interface aux organisateurs d'un championnat e-sport afin d'afficher les classements, calendriers et line-up des différentes saisons",
+    details:
+      "Il a été réalisé en <strong>PHP</strong> à partir d'une architecture <strong>MVC</strong>.<br/><strong>Composer</strong> a été utilisé comme gestionnaire de dépendance pour AltoRouter et AltoDispatcher.",
+  },
+  {
     id: 3,
+    name: 'vbr',
+    title:
+      '<span>Virtual Best Racing</span> - <strong>Javascript</strong>, <strong>Single Page Application</strong>',
+    url: 'https://precious-wash.surge.sh',
+    description:
+      'Ce site sert de vitrine pour la team e-sport "Virtual Best Racing" afin de partager ses succés, présenter ses membres et vendre des articles',
+    details:
+      'Il a été réalisé avec <strong>REACT</strong>. Ce site est en cours de construction',
+  },
+  {
+    id: 4,
     name: 'taskmanager',
     title:
       '<span>TaskManager</span> - <strong>Laravel</strong>, <strong>Javascript</strong>, <strong>Single Page Application</strong>',
