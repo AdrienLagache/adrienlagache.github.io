1a8914fe63678edc66b2cc6cf97d8fcbc29ce629
AdrienLagache <lagacheadrien64@gmail.com>
2025-06-20 15:45:42 +0200

    [TEST] let next.config.ts choose build path for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // basePath: '/adrienlagache.github.io',
-  // assetPrefix: '/adrienlagache.github.io/',
   images: {
     unoptimized: true
   }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
